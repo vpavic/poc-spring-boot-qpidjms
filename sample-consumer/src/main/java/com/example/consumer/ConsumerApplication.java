2f5950784359d0df48305140cac06ba6a034c9f0
Vedran Pavic <vedran@vedranpavic.com>
2026-04-15 22:25:22 +0200

    Disable proxy bean methods

--- sample-consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ sample-consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -3,7 +3,7 @@ package com.example.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 class ConsumerApplication {
 
 	static void main(String[] args) {
--- sample-producer/src/main/java/com/example/producer/ProducerApplication.java
+++ sample-producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -3,7 +3,7 @@ package com.example.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 class ProducerApplication {
 
 	static void main(String[] args) {
